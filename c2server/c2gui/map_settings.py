c7f3f57d868d304bbd50c8dc0dbc3feb4044f084
A Anspoka <aa934@pc557s.cs.york.ac.uk>
2016-05-11 12:22:41 +0100

    the map now responds to submitting search coordinates by colouring appropriate regions

---
+++ c2server/c2gui/map_settings.py
@@ -0,0 +1,10 @@
+from decimal import Decimal
+
+MIN_X = Decimal(0.000)
+MIN_Z = Decimal(0.000)
+MAX_X = Decimal(52.596)
+MAX_Z = Decimal(62.424)
+REG_NUM_Z = 10
+REG_NUM_X = 8
+REG_WIDTH = MAX_Z / REG_NUM_Z
+REG_HEIGHT = MAX_X / REG_NUM_X
