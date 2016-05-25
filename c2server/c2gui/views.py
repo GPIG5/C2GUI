84bb88062b1b6b09681366a26d42546f4c55cc1b
nasteidzha <nasteidzha@gmail.com>
2016-05-25 14:40:43 +0100

    Commented out the call to celery task that breaks everything.

--- c2server/c2gui/views.py
+++ c2server/c2gui/views.py
@@ -49,7 +49,7 @@ def send_search_coord(request):
     mes = DeployMesh("c2", "c2", Space(bottomleft, topright))
     c = Communicator()
     c.send(mes)
-    send_search_area_coord.delay(coordinates)
+    #send_search_area_coord.delay(coordinates)
     return HttpResponse(simplejson.dumps({"ids": ids, "headline": new_event.headline, "text": new_event.text, "timestamp": {"year": new_event.timestamp.year,
                               "month": new_event.timestamp.month,
                               "day": new_event.timestamp.day,
