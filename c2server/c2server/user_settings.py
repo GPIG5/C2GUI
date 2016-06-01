9cbe449a9d1e0350de4443708663af52561122a4
tapuu <mjm540@york.ac.uk>
2016-06-01 16:10:56 +0100

    fixing stuff

--- c2server/c2server/user_settings.py
+++ c2server/c2server/user_settings.py
@@ -2,9 +2,9 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # if you are using sqlite,
-        'NAME': 'c2', # the name of the database,
-        'USER': 'c2user', # the name of the user
-        'PASSWORD': 'test', # the password of the user
+        'NAME': 'postgres', # the name of the database,
+        'USER': 'mjm540', # the name of the user
+        'PASSWORD': '', # the password of the user
         'HOST': 'localhost', # the database host
         'PORT': '', # the database port
     }
